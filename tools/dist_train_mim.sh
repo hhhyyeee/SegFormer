5a3e2b9217479215fb764481b56a9dd4f2815366
hhhyyeee <changeling626@gmail.com>
2024-05-03 12:36:17 +0000

    240503: Add tools/dist_train_mim.sh

---
+++ tools/dist_train_mim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+GPUS=$2
+PORT=${PORT:-29500}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/train_mim.py $CONFIG --wandb 0 --pid 2 --launcher pytorch ${@:3}
